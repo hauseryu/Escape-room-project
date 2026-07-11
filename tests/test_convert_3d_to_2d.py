8b0ff60c2d4b0222fe2e0739bc7885822af1b7c2
Terumi Hauser <terumi.hauser@gmx.de>
2026-07-11 17:54:21 +0200

    tests: tests were adapted to run successully

--- tests/test_convert_3d_to_2d.py
+++ tests/test_convert_3d_to_2d.py
@@ -4,15 +4,30 @@ import sys
 
 sys.path.insert(0, str(Path(__file__).resolve().parents[1]))
 
-from src.escape_room.convert_3d_to_2d import compute_2d_coordinates
+from src.escape_room.convert_3d_to_2d import compute_2d_coordinates, convert_polygon_coordinates
 
 
 class Convert3dTo2dTest(unittest.TestCase):
-    def test_projects_center_point_to_screen_center(self):
-        self.assertEqual(compute_2d_coordinates(0, 0, 10, 800, 600), (400, 300))
+    def test_projects_point_with_world_offset_and_perspective(self):
+        x, y = compute_2d_coordinates(0, 0, 10, 800, 600)
+
+        self.assertAlmostEqual(x, 309.0909090909091)
+        self.assertAlmostEqual(y, 368.1818181818182)
 
     def test_projects_point_with_perspective(self):
-        self.assertEqual(compute_2d_coordinates(2, 1, 10, 800, 600), (430, 315))
+        x, y = compute_2d_coordinates(2, 1, 10, 800, 600)
+
+        self.assertAlmostEqual(x, 354.54545454545456)
+        self.assertAlmostEqual(y, 322.72727272727275)
+
+    def test_convert_polygon_coordinates_keeps_color_and_converts_four_points(self):
+        coordinates = convert_polygon_coordinates([
+            ["white", (0, 0, 0), (8, 0, 0), (8, 3, 4), (0, 3, 4)]
+        ])
+
+        self.assertEqual(len(coordinates), 1)
+        self.assertEqual(coordinates[0][0], "white")
+        self.assertEqual(len(coordinates[0][1:]), 8)
 
 
 if __name__ == "__main__":
