d8297e09cdb93be34a44eace0fa35756152c4603
hauseryu <147617811+hauseryu@users.noreply.github.com>
2026-08-05 21:53:12 +0200

    feat: display riddle when clicking on the picture

---
+++ src/escape_room/riddle_generator.py
@@ -0,0 +1,42 @@
+from google import genai
+from google.genai import types 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key = os.getenv("API_KEY")
+
+if not api_key:
+    raise ValueError("API_KEY is not set. Please add it to your .env file.")
+
+
+client = genai.Client(api_key=api_key)
+
+def generate_riddle():
+    config = types.GenerateContentConfig(
+        max_output_tokens=200,
+    )
+
+    response = client.models.generate_content(
+        model="gemini-3.5-flash-lite",
+        contents="""
+        Erstelle ein kurzes Rätsel.
+
+        Antworte exakt in diesem Format:
+
+        Rätsel:
+        ...
+
+        A)
+        B)
+        C)
+        D)
+
+        Richtige Antwort:
+        ...
+        """,
+        config=config
+    )
+
+    return response.text
